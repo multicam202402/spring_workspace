7ed8c3b4cafa9ecda37f35674dd32087a3bcd93b
multicampus202402 <multicam202402@gmail.com>
2024-04-04 17:40:30 +0900

    금일수업

--- SpringProject/src/com/sds/springproject/controller/RegistController.java
+++ SpringProject/src/com/sds/springproject/controller/RegistController.java
@@ -3,15 +3,12 @@ package com.sds.springproject.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import com.sds.springproject.domain.Bio;
 import com.sds.springproject.domain.Member;
-import com.sds.springproject.model.bio.BioDAO;
-import com.sds.springproject.model.member.MemberDAO;
-import com.sds.springproject.mybatis.MybatisManager;
+import com.sds.springproject.model.member.MemberService;
 
 //회원등록 요청을 처리하는 하위 컨트롤러 (3단계,4단계(선택) 수행함 )
 public class RegistController implements Controller{
@@ -19,23 +16,30 @@ public class RegistController implements Controller{
 	//따라서 SqlSession 의 취득은 현재 하위 컨트롤러가 취득하여, 같은 SqlSession을 DAO들의 
 	//insert() 메서드의 매개변수로 나누어주자! , SqlSession을 모아놓은 객체인 .SqlSessionFactory를
 	//가진 객체는 MybatisManager이므로, 이 하위 컨트롤러가 MybatisManager를 보유하도록 하자
+	/*
+	 * 컨트롤러는 컨트롤러일 뿐, 모델 영역의 자세한 업무를 절대로 부담해서는 안됀다!!
 	private MybatisManager manager;
-	
 	public void setManager(MybatisManager manager) {
 		this.manager = manager;
 	}
-	
-	
 	private MemberDAO memberDAO; //mybatisDAO 올지  hibernateDAO 올지 개발시 선택사항..
 	private BioDAO bioDAO;
-
+	 */
 	//DI로 주입을 받으려면,  setter메서드를 준비해놓아야 스프링 컨테이너가 넣어준다
+	/*
 	public void setMemberDAO(MemberDAO memberDAO) {
 		this.memberDAO = memberDAO;
 	}
 	
 	public void setBioDAO(BioDAO bioDAO) {
 		this.bioDAO = bioDAO;
+	}
+	*/
+	
+	private MemberService memberService;
+
+	public void setMemberService(MemberService memberService) {
+		this.memberService = memberService;
 	}
 	
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -45,35 +49,19 @@ public class RegistController implements Controller{
 		member.setName(request.getParameter("name"));
 		member.setEmail(request.getParameter("email"));
 		
-		//DAO들에게 일 시키기 전에, SqlSession을 취득하여 분배해주자 
-		SqlSession sqlSession = manager.getSqlSession();
+		//3단계: 모델에게 일 시킨다 (세세히 시키지 말고, 추상적으로 시키자)
+		Bio bio = new Bio();
+		bio.setBlood(request.getParameter("blood"));
+		bio.setHeight(Float.parseFloat(request.getParameter("height")));
+		bio.setWeight(Float.parseFloat(request.getParameter("weight")));
+		bio.setMember(member); //회원 신상정보 대입
 		
-		System.out.println("회원 테이블에 insert 한 이전 "+member.getMember_idx());
-		int result = memberDAO.insert(sqlSession, member);//3단계: 알맞는 모델 객체에 일 시킨다
-		
-		//Member 테이블에 insert  후 곧바로, 이  insert 의해 생성된  member_idx 를 가져와야 한다..
-		//그래야 Bio 테이블에 fk에 값을 넣을 수 있다..
-		System.out.println("회원 테이블에 insert 한 직후 "+member.getMember_idx());
-		
+		int result=0;
+		result = memberService.regist(bio);//등록해줘!!
 		
 		ModelAndView mav = new ModelAndView();
 		
 		if(result>0) {
-			//Member 신상정보가 등록되면, 바이오 정보도 넣어주자
-			
-			Bio bio = new Bio(); //empty 상태 생성
-			bio.setBlood(request.getParameter("blood")); //혈액형
-			bio.setHeight(Float.parseFloat(request.getParameter("height")));//회원의 신장  값
-			bio.setWeight(Float.parseFloat(request.getParameter("weight")));//회원 몸무게 
-			bio.setMember(member);
-			
-			result = bioDAO.insert(sqlSession , bio);//신체 정보도 등록
-			
-			if(result >0) {
-				//둘다 성공했으므로, 트랜잭션을 커밋하다
-				sqlSession.commit();
-			}
-			manager.release(sqlSession); //반납
 			
 			//회원 목록 보여주자 ( 요청을 끊고 redirect)
 			mav.setViewName("redirect:/member/list");

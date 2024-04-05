14a6c8aeb77be0450c0ea67b5ae18fb054d77c27
multicampus202402 <multicam202402@gmail.com>
2024-04-05 11:59:52 +0900

    금일수업

--- Spring/WebContent/WEB-INF/board/list.jsp
+++ Spring/WebContent/WEB-INF/board/list.jsp
@@ -1,4 +1,8 @@
 <%@ page contentType="text/html;charset=UTF-8"%>
+<%
+	String boardList = (String)request.getAttribute("boardList");
+	out.print(boardList);
+%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
--- Spring/src/com/sds/spring/controller/BoardController.java
+++ Spring/src/com/sds/spring/controller/BoardController.java
@@ -1,8 +1,10 @@
 package com.sds.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 //DefaultAnnotaionHandlerMapping이 , 아래의 클래스를 하위 컨트롤러로 발견하게끔, 어노테이션으로 표시를 해야 한다
 @Controller
@@ -11,9 +13,13 @@ public class BoardController{
 	//앞으로드는 게시판과 관련된 요청을 이 하나의 클래스에서 담당하되, 각각의 요청은 
 	//메서드로 처리한다.. 
 	@RequestMapping(value="/board/list", method=RequestMethod.GET)
-	public String getList() {
+	public String getList(Model model) {
 		System.out.println("목록요청 받음");
-		return null;
+		
+		model.addAttribute("boardList", "게시물 목록");
+	
+		//View 객체를 따로 만들지 말고, 그냥 String으로 뷰의 이름을 저장하면 자동으로 View 객체에 담김 
+		return "board/list";
 	}
 	
 }

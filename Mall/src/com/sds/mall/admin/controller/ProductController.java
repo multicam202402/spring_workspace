d7b3aeb9d87d41bd5e916a997f37256ad4c08ba1
multicampus202402 <multicam202402@gmail.com>
2024-04-09 13:52:16 +0900

    금일수업

--- Mall/src/com/sds/mall/admin/controller/ProductController.java
+++ Mall/src/com/sds/mall/admin/controller/ProductController.java
@@ -1,13 +1,16 @@
 package com.sds.mall.admin.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sds.mall.domain.Product;
 import com.sds.mall.model.product.TopCategoryService;
@@ -44,6 +47,21 @@ public class ProductController {
 		System.out.println("사이즈 수는 "+size_name.length);
 		
 		//Product 안에 들어있는 photo 를 꺼내어, 파일명 출력해보기!
+		MultipartFile photo = product.getPhoto();
+		String filename = photo.getOriginalFilename();//업로드 된 파일명
+		
+		System.out.println("파일명은 "+filename);
+		
+		//서버의 디렉토리 중 하나를 정해서, 그 안에 업로드된 정보를 파일로 저장시키자(현재까지는 메모리상의 정보일 뿐이다..)
+		try {
+			photo.transferTo(new File("D:/spring_workspace/Mall/WebContent/resources/product_img/"+filename)); //지정된 경로와 원하는 파일명으로 저장
+			System.out.println("업로드 성공");
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 		return null;
 	}

98a0f80f45ce5a615e62aa776460563a9f470a57
multicampus202402 <multicam202402@gmail.com>
2024-04-09 15:34:30 +0900

    금일수업

--- Mall/src/com/sds/mall/model/product/ProductServiceImpl.java
+++ Mall/src/com/sds/mall/model/product/ProductServiceImpl.java
@@ -2,14 +2,29 @@ package com.sds.mall.model.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.sds.mall.domain.Product;
+import com.sds.mall.model.common.FileManager;
 
+@Service
 public class ProductServiceImpl implements ProductService{
-
-	@Override
+	
+	@Autowired
+	private ProductDAO productDAO; //db 담당
+	
+	@Autowired
+	private FileManager fileManager; //업로드 담당 객체
+	
+	//등록 업무 = 파일업로드 + db insert 
 	public void regist(Product product) {
-		// TODO Auto-generated method stub
 		
+		//파일업로드 시키기 
+		fileManager.save(product);
+		
+		//db insert 시키기 
+		productDAO.insert(product);
 	}
 
 	@Override

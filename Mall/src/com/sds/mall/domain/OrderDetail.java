75d2475a4d861dba4ac5dd8dd451202c77038023
multicampus202402 <multicam202402@gmail.com>
2024-04-19 15:13:58 +0900

    금일수업

--- Mall/src/com/sds/mall/domain/OrderDetail.java
+++ Mall/src/com/sds/mall/domain/OrderDetail.java
@@ -7,6 +7,7 @@ public class OrderDetail {
 	private int orderdetail_idx;
 	private OrderSummary orderSummary; //어떤 주문에 대한 것인지..
 	private Product product;
+	private int ea;
 	private String product_name;
 	private int price;
 }
